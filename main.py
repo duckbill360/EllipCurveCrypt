5af2e8e06fdca4b2865f06c2f75cd6f8eab0c9f3
Po-Wei Cheng <a821023a821023@yahoo.com.tw>
2017-05-15 15:06:52 +0800

    Finished Encryption

--- main.py
+++ main.py
@@ -22,16 +22,45 @@ def add_space(string):
 
 
 if __name__ == '__main__':
+    # ENCRYPTION
     print('<EC-ElGamal encryption>')
-    M_str = "2923BE84 E16CD6AE 529049F1 F1BBE9EB B3A6DB"
+    M_str = "E1DB763C 99248E66 0A4801A9 A973A1A3 6B5E93"
     M = int(delete_space(M_str), 16)
     print('Plaintext M =', add_space(format(M, 'x')))
     Mx, My = ECC.data_embedding(M)
     print('Mx =', add_space(format(Mx, 'x')))
     print('My =', add_space(format(My, 'x')))
-    P = point.Point(Mx, My)
+    Pm = point.Point(Mx, My)
 
+    Pa = int(delete_space('03 7E3966DF 631F4871 3E61F0B7 0E1B5F77 C8A5B41B'), 16)
+    nk = int(delete_space('5ED7BB12 35C1F0DD D7158C83 B44EADFD F3CBC541'), 16)
+
+    # G is the given point
     G = point.Point(ECC.Gx, ECC.Gy)
-    A = point.Point(ECC.a, ECC.b)
-    G = G.add(A)
-    G.show()
+    Pk = G.scalar_mul(nk)
+    # select even or odd
+    selector = Pa // 0x10000000000000000000000000000000000000000    # Pa = 2 or 3
+    Pa_x = Pa % 0x10000000000000000000000000000000000000000
+
+    # find the whole Pa
+    value = (Pa_x ** 3 + ECC.a * Pa_x + ECC.b) % ECC.p
+    r = ECC.sqrt_p_3_mod_4(value, ECC.p)
+    Pa_y = 0
+    if selector == 2 and r % 2 == 0:
+        Pa_y = r
+    elif selector == 2 and r % 2 == 1:
+        Pa_y = ECC.p - r
+    elif selector == 3 and r % 2 == 0:
+        Pa_y = ECC.p - r
+    elif selector == 3 and r % 2 == 1:
+        Pa_y = r
+    Pa = point.Point(Pa_x, Pa_y)
+
+    Pb = Pm.add(Pa.scalar_mul(nk))
+    print('Cm = {Pk, Pb} = {', add_space(format(Pk.x, 'x')), ',', add_space(format(Pb.x, 'x')), '}')
+
+    # DECRYPTION
+
+    # G = point.Point(ECC.Gx, ECC.Gy)
+    # A = point.Point(ECC.a, ECC.b)
+    # print(add_space(format(pow(ECC.a - ECC.Gx, ECC.p - 2, ECC.p), 'x')))
--- point.py
+++ point.py
@@ -15,7 +15,9 @@ class Point:
     # point addition
     def add(self, input):
         output = Point(0, 0)
-        lamb = (((input.y - self.y) % ECC.p) // ((input.x - self.x) % ECC.p)) % ECC.p
+        denominator = (input.x - self.x) % ECC.p
+        numerator = (input.y - self.y) % ECC.p
+        lamb = (pow(denominator, ECC.p - 2, ECC.p) * numerator) % ECC.p
         output.x = (lamb ** 2 - self.x - input.x) % ECC.p
         output.y = (ECC.p - self.y + lamb * ((self.x - output.x) % ECC.p)) % ECC.p
 
@@ -24,25 +26,38 @@ class Point:
     # point doubling
     def doubling(self):
         output = Point(0, 0)
-        lamb = (((3 * (self.x ** 2) + ECC.a) % ECC.p) // ((2 * self.y) % ECC.p)) % ECC.p
+        denominator = (2 * self.y) % ECC.p
+        numerator = (3 * (self.x ** 2) + ECC.a) % ECC.p
+        lamb = (pow(denominator, ECC.p - 2, ECC.p) * numerator) % ECC.p
         output.x = (lamb ** 2 - 2 * self.x) % ECC.p
         output.y = (ECC.p - self.y + lamb * ((self.x - output.x) % ECC.p)) % ECC.p
 
         return output
 
     def scalar_mul(self, b):
-        output = Point(0, 0)
-        D = Point(0, 0)
-        str = bin(b)[2:]
+        D = Point(-1, -1)
+        string = bin(b)[2:]
 
-        for i in str:
-            if D.x != 0:
+        for i in string:
+            if D.x != -1:
                 D = D.doubling()
             else:
-                if int(i) == 1:
+                D = Point(-1, -1)
+
+            if int(i) == 1:
+                if D.x == -1:
+                    D = self
+                else:
                     D = D.add(self)
+
         return D
 
     def show(self):
-        print(format(self.x, 'x'))
-        print(format(self.y, 'x'))
+        print('x :', add_space(format(self.x, 'x')))
+        print('y :', add_space(format(self.y, 'x')))
+
+
+def add_space(string):
+    string = string[::-1]
+    string = ' '.join(string[i:i + 8] for i in range(0, len(string), 8))
+    return string[::-1]

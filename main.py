7e8b3b2ecb379763a381701ca5ea929fc2237581
Po-Wei Cheng <a821023a821023@yahoo.com.tw>
2017-05-15 16:52:09 +0800

    Completed

--- main.py
+++ main.py
@@ -24,16 +24,13 @@ def add_space(string):
 if __name__ == '__main__':
     # ENCRYPTION
     print('<EC-ElGamal encryption>')
-    M_str = "110BA66C C954BE96 3A7831D9 D9A3D1D3 9B8EC3"
-    M = int(delete_space(M_str), 16)
-    print('Plaintext M =', add_space(format(M, 'x')))
+    M = int(delete_space(input('Plaintext M = ')), 16)
+    Pa_x = int(delete_space(input('Pa = ')), 16)
+    nk = int(delete_space(input('nk = ')), 16)
     Mx, My = ECC.data_embedding(M)
     print('Mx =', add_space(format(Mx, 'x')))
     print('My =', add_space(format(My, 'x')))
     Pm = point.Point(Mx, My)
-
-    Pa_x = int(delete_space('02 7AB13D6D 69847A9C CE9A84E5 DB1BDDD8 7F11F38C'), 16)
-    nk = int(delete_space('8E07EB42 65F1200D 0745BCB3 E47EDD2D 23FBF571'), 16)
 
     # G is the given point
     G = point.Point(ECC.Gx, ECC.Gy)
@@ -60,10 +57,10 @@ if __name__ == '__main__':
     print('Cm = {Pk, Pb} = {', add_space(format(Pk.x, 'x')), ',', add_space(format(Pb.x, 'x')), '}')
 
     # DECRYPTION
-    print('<EC-ElGamal encryption>')
-    Pk = int(delete_space('02 7AF4ED0D 220D9482 424E72FE 5A375C6B FC2B0743'), 16)
-    Pb = int(delete_space('03 015A7D66 7CDA436F 401E6156 9109D753 ECD1F0B1'), 16)
-    na = int(delete_space('246FF426 810C46F5 04EE9F2F C69BFA35 B02BA373'), 16)
+    print('\n<EC-ElGamal encryption>')
+    Pk = int(delete_space(input('Pk = ')), 16)
+    Pb = int(delete_space(input('Pb = ')), 16)
+    na = int(delete_space(input('na = ')), 16)
     selector_Pk = Pk // 0x10000000000000000000000000000000000000000  # Pa = 2 or 3
     selector_Pb = Pb // 0x10000000000000000000000000000000000000000  # Pa = 2 or 3
     Pk_x = Pk % 0x10000000000000000000000000000000000000000
@@ -102,4 +99,3 @@ if __name__ == '__main__':
 
     Pm = Pb.add(Pk)
     print('Plaintext =', add_space(format(Pm.x, 'x'))[:-2])
-
